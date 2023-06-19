803dc8104a85d528d4242a019524a8d8ff2951a5
bocahkampung212 <org.kmpg1101@gmail.com>
2023-06-19 10:18:40 +0700

    updatetampilan

--- lib/views/Dashbord.dart
+++ lib/views/Dashbord.dart
@@ -9,70 +9,132 @@ class Dashbord extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromRGBO(72, 238, 130, 1),
-        body: Stack(children: [
-          Image.asset(
-            'assets/background.png',
-          ),
-          Padding(
-              padding: const EdgeInsets.fromLTRB(20, 60, 20, 0),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(60),
-                    child: Image.asset('assets/logo.png', width: 60),
-                  ),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('Hi, ryo wikky'),
-                      Text('ryowikky@gmail.com')
-                    ],
-                  )
-                ],
-              )),
-          Row(
+        body: SingleChildScrollView(
+          child: Column(
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 380, left: 20, right: 20),
-              ),
-              Text('Informasi'),
-              Padding(padding: EdgeInsets.only(top: 380, left: 50, right: 100)),
-              TextButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (bc) {
-                      return Informasi();
-                    }));
-                  },
-                  child: Text('Selengkapnya')),
+              Stack(children: [
+                Container(
+                    height: 100,
+                    child: SizedBox(
+                    height: 50,),
+                ),
+                Image.asset(
+                  'assets/background.png',
+                ),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 60, 20, 0),
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(60),
+                          child: Image.asset('assets/logo.png', width: 60),
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text('Hi, ryo wikky'),
+                            Text('ryowikky@gmail.com')
+                          ],
+                        )
+                      ],
+                    )),
+            
+              ]),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+                    ),
+                    Text('Informasi'),
+                    Padding(padding: EdgeInsets.only(top: 20, left: 50, right: 100)),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (bc) {
+                            return Informasi();
+                          }));
+                        },
+                        child: Text('Selengkapnya')),
+                  ],
+                ),
+                Container(
+                    child: Card(
+                        margin: EdgeInsets.only(
+                          right: 25,
+                          left: 25,
+                        ),
+                        color: Color.fromRGBO(238, 233, 233, 0.466),
+                        child: SizedBox(
+                          height: 105,
+                          width: double.infinity,
+                        ),
+                        
+                    ),
+        
+                ),
+               
+               Container(
+                  padding:EdgeInsets.only(top: 10, left: 10,
+                  right: 10),
+                  child: Wrap(
+                          children: [
+                            tombolMenu(
+                            icon: Icon(Icons.calendar_month, size: 25,),
+                            ),
+                            tombolMenu(
+                              icon: Icon(Icons.inbox, size: 25,),
+                            ),
+                            tombolMenu(
+                              icon: Icon(Icons.location_city, size: 25,),
+                            ),
+                            tombolMenu(
+                              icon: Icon(Icons.comment, size: 25,),
+                            ),
+                      
+                          ],
+                        ),
+                     
+                ),
+                Container(
+                    child: Card(
+                  margin: EdgeInsets.only(
+                    top: 6,
+                    right: 25,
+                    left: 25,
+                  ),
+                  color: Color.fromRGBO(238, 233, 233, 0.466),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 105,
+                  ),
+                  
+                ),
+        
+                ),
             ],
           ),
-          Container(
-              child: Card(
-            margin: EdgeInsets.only(
-              top: 200,
-              right: 25,
-              left: 25,
-            ),
-            color: Color.fromRGBO(238, 233, 233, 0.466),
-            child: SizedBox(
-              width: 1000,
-              height: 150,
-            ),
-          )),
-          Container(
-              child: Card(
-            margin: EdgeInsets.only(
-              top: 400,
-              right: 25,
-              left: 25,
-            ),
-            color: Color.fromARGB(183, 59, 182, 240),
-            child: SizedBox(
-              width: 1500,
-              height: 150,
-            ),
-          ))
-        ]));
+        ));
+  }
+}
+
+class tombolMenu extends StatelessWidget {
+  final Widget? icon;
+  const tombolMenu({
+    super.key,
+    this.icon
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20)
+      ),
+      margin: EdgeInsets.only(top:5, right: 20, left: 20),
+      child: Padding(padding: const EdgeInsets.all(15.0),
+      child: this.icon
+      )
+    );
   }
 }
